420228ed61c70e6c775db9dd97c709927b79896f
Elia Cavallazzi <elia.cavallazzi@nttdata.com>
2020-04-05 15:32:51 +0200

    added bootstrap with 5 fruit shop categories

--- src/main/java/guru/springfamework/domain/Category.java
+++ src/main/java/guru/springfamework/domain/Category.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 /**
  * Created by jt on 9/24/17.
@@ -19,4 +20,32 @@ public class Category {
     private Long id;
     private String name;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+        Category category = (Category) o;
+        return getId().equals(category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
